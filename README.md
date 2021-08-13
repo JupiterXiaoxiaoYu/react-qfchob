808a22f28bd2fc472d871cfbe72994bb96d98601
JupiterXiaoxiaoYu <s2068649@ed.ac.uk>
2021-08-13 13:05:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qfchob
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qfchob)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+
+  return (
+    <div className="App">
+        <header className="App-header" id="root">
+          <h1>你好</h1>
+          <div>
+        Please wait while we connect to your account.
+        If this takes more than a few seconds, there may be something wrong.
+      </div>
+        </header>
+      </div>
+  );
+}
